318a11293a402691a92547e61a9b693c664b7750
Alain <alarseneault@hotmail.com>
2022-11-16 21:15:50 -0500

    init

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+
+console.log(toggleButton);
+console.log(navbarLinks);
+
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+    console.log("test");
+});
